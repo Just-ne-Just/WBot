a7d88081acdec911cc39845ef57e941e4df106db
Andrew <semakov_2002@mail.ru>
2022-01-03 14:26:58 +0300

    commands

--- src/bot/commands.py
+++ src/bot/commands.py
@@ -4,6 +4,7 @@ import warnings
 import urllib
 import time
 import os
+import requests
 
 bot = telebot.TeleBot("5040031474:AAEYMlFvTE54deHlDGvWRIEOUKoK8XeY23o")
 
@@ -11,5 +12,31 @@ bot = telebot.TeleBot("5040031474:AAEYMlFvTE54deHlDGvWRIEOUKoK8XeY23o")
 def hello(message):
         bot.send_message(message.chat.id,
                          "Добрый день!")
+
+@bot.message_handler(commands=['help'])
+def hello(message):
+        bot.send_message(message.chat.id,
+                         "Список всех команд")
+
+@bot.message_handler(commands=['article'])
+def hello(message):
+        bot.send_message(message.chat.id,
+                         "Артикул")
+
+@bot.message_handler(commands=['newexcel'])
+def hello(message):
+        bot.send_message(message.chat.id,
+                         "Присоединить новую ексель табличку")
+
+@bot.message_handler(commands=['codeword'])
+def hello(message):
+        bot.send_message(message.chat.id,
+                         "Ключ для выкупа")
+
+@bot.message_handler(commands=['buy'])
+def hello(message):
+        bot.send_message(message.chat.id,
+                         "Выкупаем")
+
 
 bot.polling(none_stop=True)
